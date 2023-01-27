871b081791a7f6f3a0b6fc5ddd7ba9dbd36e0a4d
Ali <ali.ozgur@example.com>
2023-01-27 19:28:23 +0300

    added contextApı

--- src/context/context.js
+++ src/context/context.js
@@ -1,3 +1,49 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useContext } from "react";
 
 const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+    const [sidebarOpen, setSidebarOpen] = useState(true)
+    const [modalOpen, setModalOpen] = useState(true);
+
+    const openSidebar = ()=> {
+        setSidebarOpen(true);
+    }
+
+      const closeSidebar = () => {
+        setSidebarOpen(false);
+      };
+
+    const openModal = () => {
+          setModalOpen(true);
+        };
+
+    const closeModal = () => {
+          setModalOpen(false);
+        };
+
+
+    return (
+      <AppContext.Provider
+        value={{
+          sidebarOpen,
+          modalOpen,
+          openSidebar,
+          closeSidebar,
+          openModal,
+          closeModal,
+        }}
+      >
+        {" "}
+        {children}
+      </AppContext.Provider>
+    );
+};
+
+export default AppProvider
+
+// yaptığımız contexti useContext Hook ile custom hook olarak tanımlıyoruz.
+// Hook lar mutlaka use ile başlar yoksa çalışmaz.
+export const useGlobalContext = ()=>{
+    return useContext(AppContext)
+}
