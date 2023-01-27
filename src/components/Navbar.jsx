4d6ab9ecc7b12cb95ab88f932f482f654d7cfc57
Ali <ali.ozgur@example.com>
2023-01-27 21:43:09 +0300

    edit navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import {useGlobalContext} from "../context/context" // costum Hook umuzu import ediyoruz.
+import team from "../helper/team.jpg" // logomozu import ediyoruz.
+
+import {FaBars} from "react-icons/fa"
 
 const Navbar = () => {
+    const {openSidebar,openModal,closeModal} = useGlobalContext()
   return (
-    <div>Navbar</div>
-  )
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={team} alt="logo" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">Ürünlerimiz</button>
+          </li>
+          <li>
+            <button className="link-btn">Geliştirmelerimiz</button>
+          </li>
+          <li>
+            <button className="link-btn">Şirketimiz</button>
+          </li>
+        </ul>
+        <div>
+          {" "}
+          <button className="btn sign-btn">Giriş Yap </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
